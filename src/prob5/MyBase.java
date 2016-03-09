4b0c8d9b44696d0cfc4560e774da1971c562260e
yjhwo <yjhwo@naver.com>
2016-03-09 10:06:36 +0900

    2nd

--- src/prob5/MyBase.java
+++ src/prob5/MyBase.java
@@ -7,23 +7,31 @@ public class MyBase extends Base{
 		System.out.println("낮에는 열심히 일하자!");
 	}
 
-	@Override
-	public void night() {
+	public void afternoon() {
 		System.out.println("오후도 낮과 마찬가지로 열심히 일해야 합니다.");
 	}
 
 	
-
 	@Override
-	public void service(String state) {
-		if( state.equals( "낮" ) ) {
-			day();
-		}else if(state.equals("밤")) {
-			super.night();
-		}else{
-			night();
-		}
+	public void service(String state) {	// 선생님
+		if(state.equals("오후"))
+			afternoon();
+		else
+			super.service(state);
 	}
+
+	
+//
+//	@Override
+//	public void service(String state) {
+//		if( state.equals( "낮" ) ) {
+//			day();
+//		}else if(state.equals("밤")) {
+//			super.night();
+//		}else{
+//			night();
+//		}
+//	}
 
 
 	
